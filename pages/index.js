158a39eb28c1c110b13982d169eb053b0f45f969
Max Schoening <max@github.com>
2020-10-14 15:33:35 +0000

    First scaffolding of wonderful web app

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+const Index = () => (<div>
+  <table>
+    <tr>
+      <th>Number</th>
+      <th>Name</th>
+      <th>Manage</th>
+    </tr>
+    <tr>
+      <td>1</td>
+      <td>Pikachu</td>
+      <td>
+        <button>Edit</button>
+        <button>Delete</button>
+      </td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Snorlax</td>
+      <td>
+        <button>Edit</button>
+        <button>Delete</button>
+      </td>
+    </tr>
+  </table>
+
+  <div>
+    <button>Add</button>
+  </div>
+</div>)
+export default Index
