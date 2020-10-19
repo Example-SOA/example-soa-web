1ccf6324e45fa2243b23d412eed52f7d34693179
Max Schoening <max@github.com>
2020-10-19 13:10:42 -0700

    Move Dockerfile to the top level

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,16 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 
 const Index = () => {
-  const [items, setItems] = useState([])
+  const [items, setItems] = useState([]);
 
   useEffect(() => {
     fetch(`${process.env.NEXT_PUBLIC_API_URL}`)
-      .then(res => res.json())
-      .then(res => {
-        setItems(res)
+      .then((res) => res.json())
+      .then((res) => {
+        setItems(res);
       })
-      .catch(err => console.error(err))
-  }, [])
+      .catch((err) => console.error(err));
+  }, []);
 
   return (
     <div>
@@ -19,17 +19,15 @@ const Index = () => {
           <th>ID</th>
           <th>Title</th>
         </tr>
-        {
-          items.map(item => (
-            <tr>
-              <td>{item.id}</td>
-              <td>{item.title}</td>
-            </tr>
-          ))
-        }
+        {items.map((item) => (
+          <tr>
+            <td>{item.id}</td>
+            <td>{item.title}</td>
+          </tr>
+        ))}
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
